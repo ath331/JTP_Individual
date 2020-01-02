2be75643939694ee23b2dd1da913c1841e5ae158
ath331 <kstnftk@naver.com>
2020-01-02 22:08:00 +0900

    exe용 폴더 생성

--- PacMan/PlayGame/PlayGameMain.cpp
+++ PacMan/PlayGame/PlayGameMain.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-	std::cout << "Hi" << std::endl;
+	//std::cout << "Hi" << std::endl;
 	CConsoleLogger secondConsole;
 	secondConsole.Create("secondConsole");
 	secondConsole.printf("EndKey : Enter");
