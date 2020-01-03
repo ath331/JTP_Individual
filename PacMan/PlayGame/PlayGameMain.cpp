1489a3a248e8e7d424e40ad1ac4f2681b0d86fd6
ath331 <kstnftk@naver.com>
2020-01-03 10:06:27 +0900

    Update PlayGameMain.cpp

--- PacMan/PlayGame/PlayGameMain.cpp
+++ PacMan/PlayGame/PlayGameMain.cpp
@@ -1,30 +1,6 @@
 #include <iostream>
 
-#include <boost/lexical_cast.hpp>
-#include <boost/thread/thread.hpp>
-#include <process.h>
-
-#include "ConsoleLogger/ConsoleLogger.h"
-
 int main()
 {
-	SYSTEM_INFO systemInfo;
-	GetSystemInfo(&systemInfo);
-
-	int sum = 0;
-	CConsoleLogger secondConsole;
-	secondConsole.Create("secondConsole");
-	secondConsole.printf("EndKey : Enter\n");
-
-	while (sum < 100)
-	{
-		std::string a = boost::lexical_cast<std::string>(sum);
-		secondConsole.printf(a.c_str());
-		sum++;
-	}
-
-	while (1)
-	{
-		std::cout << "aa" << std::endl;
-	}
+	std::cout << "aa" << std::endl;
 }
